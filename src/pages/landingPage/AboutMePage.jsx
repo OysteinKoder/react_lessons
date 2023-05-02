01cafeda75ee880d889540c09ffb7ead450d6011
OysteinKoder <oystein.kodehode@gmail.com>
2023-05-02 12:36:47 +0200

    added text

--- src/pages/landingPage/AboutMePage.jsx
+++ src/pages/landingPage/AboutMePage.jsx
@@ -5,6 +5,7 @@ const AboutMePage = () => {
       <p>The github repo is in the bottom</p>
       <p>Each branch of the repo covers a different topic</p>
       <p>Each topic has its own task in task.txt</p>
+      <p>Please give the repo a star on github!</p>
     </>
   );
 };
