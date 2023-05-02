cfdf521f5a8549718374f18fa7afe6e5b2fca902
OysteinKoder <oystein.kodehode@gmail.com>
2023-05-02 12:00:06 +0200

    b

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-lessons-4.0/",
+  base: "/react_lessons/",
 });
